2f7b828e9062f0c1a72bf06373a6d5001c5bd917
aditsangani <apsangani3106@gmail.com>
2026-03-28 19:07:08 -0400

    Adding the functionality for the dijkstra.h file

--- include/Dijkstra.h
+++ include/Dijkstra.h
@@ -5,4 +5,24 @@
 #ifndef P2_GROUP_33_DIJKSTRA_H
 #define P2_GROUP_33_DIJKSTRA_H
 
+#include "Graph.h"
+#include <vector>
+#include <unordered_map>
+
+struct PathResult {
+    std::vector<long long> path;
+    double totalDistance;
+    long long visitedNodes;
+    double executionTimeMs;
+    bool found;
+};
+
+class Dijkstra {
+public:
+
+    PathResult shortestPath(const Graph& g, long long src, long long dst);
+};
+
+
+
 #endif //P2_GROUP_33_DIJKSTRA_H
