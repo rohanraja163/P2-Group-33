2c764f6e5c79a3dcf5a48cd85ad98b93bc1145f2
Rohan Raja <rajarohan163@gmail.com>
2026-03-28 16:35:23 -0400

    Add DataLoader class with loadFromFile method

---
+++ src/DataLoader.cpp
@@ -0,0 +1,11 @@
+#include "DataLoader.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
+bool DataLoader::loadFromFile(const std::string& filepath, Graph& graph) {
+}
