165d39275e8379665ff5177d83ad79d6e949da0c
aditsangani <apsangani3106@gmail.com>
2026-03-28 19:09:04 -0400

    Adding the first part for the dijkstra.cpp file

--- src/Dijkstra.cpp
+++ src/Dijkstra.cpp
@@ -1,3 +1,15 @@
 //
 // Created by adits on 3/28/2026.
 //
+
+#include "Dijkstra.h"
+#include <queue>
+#include <limits>
+#include <chrono>
+#include <algorithm>
+
+PathResult Dijkstra::shortestPath(const Graph& g, long long src, long long dst) {
+    PathResult result;
+    result.found         = false;
+    result.totalDistance = std::numeric_limits<double>::infinity();
+    result.visitedNodes  = 0;
